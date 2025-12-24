58c21ba0cbb4e3d27b432b02958d8bd7eea734fa
Eric Jeong <ericy.jeong@gmail.com>
2025-12-24 01:56:48 -0800

    Added service framework

--- src/main/java/com/banksaver/BankSaver/service/ProfileService.java
+++ src/main/java/com/banksaver/BankSaver/service/ProfileService.java
@@ -1,7 +1,10 @@
 package com.banksaver.BankSaver.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+
 public class ProfileService {
 }
