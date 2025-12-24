cbdd5f932abe92e4ab82bc4c565673540ed7e253
Eric Jeong <ericy.jeong@gmail.com>
2025-12-23 21:09:43 -0800

    f

---
+++ src/main/java/com/banksaver/BankSaver/BankSaverApplication.java
@@ -0,0 +1,13 @@
+package com.banksaver.BankSaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSaverApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(BankSaverApplication.class, args);
+	}
+
+}
