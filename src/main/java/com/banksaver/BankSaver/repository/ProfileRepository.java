7d1b251d0fcbd9e7be266f3ddcf53b89e8c11e98
Eric Jeong <ericy.jeong@gmail.com>
2026-01-01 19:16:39 -0800

    Created ProfileService to register a profile

--- src/main/java/com/banksaver/BankSaver/repository/ProfileRepository.java
+++ src/main/java/com/banksaver/BankSaver/repository/ProfileRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ProfileRepository extends JpaRepository{
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
 
     // select * from tbl_profiles where email = ??
     Optional<ProfileEntity> findByEmail(String email);
--- src/main/java/com/banksaver/BankSaver/service/ProfileService.java
+++ src/main/java/com/banksaver/BankSaver/service/ProfileService.java
@@ -1,13 +1,46 @@
 package com.banksaver.BankSaver.service;
 
+import com.banksaver.BankSaver.dto.ProfileDTO;
+import com.banksaver.BankSaver.entity.ProfileEntity;
 import com.banksaver.BankSaver.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
-
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO) {
+        ProfileEntity newProfile = toEntity(profileDTO);
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO) {
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .password(profileDTO.getPassword())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity) {
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
 }
