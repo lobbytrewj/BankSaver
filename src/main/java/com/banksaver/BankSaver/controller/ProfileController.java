654c62d9ed2a8f1c1288e2c07a591d8c0285ab5c
Eric Jeong <ericy.jeong@gmail.com>
2026-01-05 16:18:51 -0800

    Created sqldatabase with postman and workbench

---
+++ src/main/java/com/banksaver/BankSaver/controller/ProfileController.java
@@ -0,0 +1,22 @@
+package com.banksaver.BankSaver.controller;
+
+import com.banksaver.BankSaver.dto.ProfileDTO;
+import com.banksaver.BankSaver.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+    private final ProfileService profileService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ProfileDTO> registerProfile(@RequestBody ProfileDTO profileDTO) {
+        ProfileDTO registeredProfile = profileService.registerProfile(profileDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredProfile);
+    }
+}
